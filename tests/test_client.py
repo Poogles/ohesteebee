c314ec323682bb637e3dab8bcf46a695b6add7e8
Sam Pegler <sam@sampegler.co.uk>
2017-02-24 01:31:11 +0000

    Create test_client.py

---
+++ tests/test_client.py
@@ -0,0 +1,35 @@
+import pytest
+
+import ohessteebee.client as client
+
+put_example = {
+    "metric": "sys.cpu.nice",
+    "timestamp": 1346846400,
+    "value": 18,
+    "tags": {
+       "host": "web01",
+       "dc": "lga"
+    }
+}
+
+
+@pytest.fixture(autouse=True)
+def patch_requests_session(mocker):
+	mocker.patch('client.requests.')
+
+
+@pytest.fixture()
+def ostb():
+	pass
+
+
+def test_client():
+	pass
+
+
+def test_get():
+	pass
+
+
+def test_put():
+	pass
